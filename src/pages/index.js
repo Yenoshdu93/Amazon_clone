ebfc247d5a28ac6272ae3657069b6ca2035f80cc
YenoshDulla <yenoshdulla9366@gmail.com>
2023-09-01 23:25:24 +0530

    icon and title added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,18 @@ import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ProductFeed from "@/components/ProductFeed";
+import Head from "next/head";
 
 const Home = ({ productsData }) => {
   return (
     <div>
+      <Head>
+        <title>Amazon a shopping Cart where you can find any thing.</title>
+        <link
+          rel="icon"
+          href="https://companieslogo.com/img/orig/AMZN-e9f942e4.png?t=1632523695"
+        />
+      </Head>
       <Header />
       <main className="text-red-50">
         <Banner />
